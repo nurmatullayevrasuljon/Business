004fa2db3505b545de2ed23ebea7e4612805ca61
Rasuljon2007 <rasuljonnurmatullayev@gmail.com>
2024-11-22 06:03:48 -0800

    add .

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+//  
+// Initialize the map
+var map = L.map('map').setView([51.5034, -0.1276], 10); // Coordinates for London
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Add a custom marker at 10 Downing Street
+var marker = L.marker([51.5034, -0.1276]).addTo(map);
+
+// Add a popup to the marker
+marker.bindPopup(
+    `
+<div class="custom-popup">
+    <strong>Business</strong><br>
+    10 Downing Street<br>
+    London<br>
+    SW1A 2AA<br>
+    United Kingdom
+</div>`
+).openPopup();
+
+
+// nubmeb //
+
+// nubmeb //
